1720671b1bbbb2986f49d54020e893dfbf636632
abrar6390 <abrark4961@gmail.com>
2023-12-27 23:24:07 +0530

    initial commit

---
+++ src/main/java/com/abrar/SprinBoorCrudByAbrarApplication.java
@@ -0,0 +1,13 @@
+package com.abrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBoorCrudByAbrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBoorCrudByAbrarApplication.class, args);
+	}
+
+}
